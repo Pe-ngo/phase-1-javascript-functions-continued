ded91d56723f96213528c2b83c407955c6fdf3d9
Pe-ngo <jamespengo95@gmail.com>
2022-06-09 19:04:27 +0300

    commit tree 3474952c732bfb32c659e6f4ff01205ac72871fb

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`) 
+}
+console.log(saturdayFun())
+
+const mondayWork = function(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+console.log(mondayWork())
+
+const wrapAdjective = function(active = '*') {
+    return function(soft = "a hard worker"){
+        return(`You are ${active}${soft}${active}!`)
+    }
+    
+}
+console.log(wrapAdjective()())
